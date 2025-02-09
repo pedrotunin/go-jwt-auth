72e6dc63a8ae3a597bcf5177b56a8c635f1342e2
Pedro Tunin <pedrotunin@gmail.com>
2025-02-09 19:30:21 -0300

    feat: add logging for password service

--- internal/services/password_service.go
+++ internal/services/password_service.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"log"
+
 	"github.com/alexedwards/argon2id"
 	"github.com/pedrotunin/jwt-auth/internal/utils"
 )
@@ -15,19 +17,24 @@ func NewPasswordService() *PasswordService {
 func (ps *PasswordService) Hash(password string) (hash string, err error) {
 	hash, err = argon2id.CreateHash(password, argon2id.DefaultParams)
 	if err != nil {
+		log.Printf("Hash: error creating password hash: %s", err.Error())
 		return "", err
 	}
+	log.Printf("Hash: hash created")
 	return hash, err
 }
 
 func (ps *PasswordService) Compare(password string, hashedPassword string) error {
 	match, err := argon2id.ComparePasswordAndHash(password, hashedPassword)
 	if err != nil {
+		log.Printf("Compare: error comparing password and hash: %s", err.Error())
 		return err
 	}
 	if !match {
+		log.Printf("Compare: password do not match with hash")
 		return utils.ErrPasswordsNotMatch
 	}
 
+	log.Printf("Compare: password and hash match")
 	return nil
 }
